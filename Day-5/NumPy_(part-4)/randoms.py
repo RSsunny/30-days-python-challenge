2ce56d152ce37483bb2343f2962d1d91069d0795
RSsunny <sunnysharif154@gmail.com>
2025-05-03 23:55:24 +0600

    Random part explore and add some file.

---
+++ Day-5/NumPy_(part-4)/randoms.py
@@ -0,0 +1,12 @@
+from numpy import random
+
+x = random.randint(100)
+y= random.rand()
+arr= random.randint(100,size=(5,3))
+arr2= random.rand(5)
+print(arr)
+print(arr2)
+print(random.choice(arr2))
+
+# Random Data Distribution
+print(random.choice([4,5,6,7,2],p=[0.3,0.5,0.1,0.1,0.0],size=100))
